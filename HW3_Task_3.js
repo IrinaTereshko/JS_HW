41c6c988407427c5f6dc9ae8694e8f7b1bcd2a21
IrinaTereshko <111395544+IrinaTereshko@users.noreply.github.com>
2024-06-03 15:48:19 +0200

    Add files via upload

---
+++ HW3_Task_3.js
@@ -0,0 +1,7 @@
+ let input_number = prompt("введите число от 0 до 9 :");
+let result;
+result = input_number*1 + (input_number+input_number)*1 +
+                                 (input_number+input_number+input_number)*1;
+    
+alert(result)
+           
